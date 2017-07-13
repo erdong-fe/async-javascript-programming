8e78a80d57b0c16f258b09e790cff55b5b86a065
RickCZG <czg18366118377@yahoo.com>
2017-07-13 13:44:53 +0800

    firecount

---
+++ fireCount.js
@@ -0,0 +1,9 @@
+var fireCount = 0;
+var start = new Date();
+var timer = setInterval(function () {
+	if(new Date() - start > 1000){
+		clearInterval(timer);
+		console.log(fireCount);
+		return;
+	}
+}, 0);
